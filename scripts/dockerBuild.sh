2cd44fb9bf6d1b6d8da58ad9f81b6f80eedec2d0
Lance Tipton <lancetipton04@gmail.com>
2022-07-19 06:13:39 -0700

    feat: Update version   * Add opts to build script   * Update ci workflows

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -3,20 +3,37 @@
 source $KEG_CLI_PATH/keg
 source scripts/logger.sh
 
-BUILD_ARGS=''
-[ "$1" == "push" ] && BUILD_ARGS="--platform linux/amd64,linux/arm64 --push" || BUILD_ARGS="--load"
-
 IMAGE_NAME=$npm_package_displayName
 IMAGE_VERSION=$npm_package_version
-IMAGE_FULL=ghcr.io/gobletqa/$IMAGE_NAME:$IMAGE_VERSION
+IMAGES_URI=ghcr.io/gobletqa/$IMAGE_NAME
+IMAGE_FULL=$IMAGES_URI:$IMAGE_VERSION
+
+BUILD_ARGS="--load"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--push)
+      logMsg "Add platforms arguments to push image"
+      BUILD_ARGS="--platform linux/amd64,linux/arm64 --push"
+      shift
+    ;;
+    -l|--local)
+      docker buildx create --name goblet &> /dev/null
+      docker buildx use goblet
+      shift
+    ;;
+    -t|--tag|--tags)
+      BUILD_ARGS="$BUILD_ARGS -t $IMAGES_URI:$2"
+      shift
+      shift
+    ;;
+    *)
+      logErr "Unknown option $1"
+      shift
+    ;;
+  esac
+done
 
 logMsg "Building image $IMAGE_FULL"
-
-# Only run if the second argument is not CI
-# This is not needed in github actions
-if [ -z "$2" ]; then
-  # Create the builder if needed, capture the output incase it already exists we don't want to exit on error
-  IGNORE=$(docker buildx create --name goblet)
-fi
-docker buildx use goblet
 docker buildx build $BUILD_ARGS -t $IMAGE_FULL .
+logMsg "Finished Building image"
